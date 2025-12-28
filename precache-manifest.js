1c8212425b2d043e69969892cd243918980f9005
gwer <gwer@users.noreply.github.com>
2025-12-28 11:25:27 +0000

    deploy: 65c1d42e6689f06107ea8aa854393aee47e53fd6

--- precache-manifest.js
+++ precache-manifest.js
@@ -1,8 +1,11 @@
 self.__PRECACHE_URLS = [
-  "chunk-kexs57e9.js",
-  "chunk-s6m4cdqa.js",
+  "chunk-6dpyqvz4.js",
   "index.html",
+  "chunk-m74g3x6g.js",
   "chunk-taey88dt.js",
   "v1/view/index.html",
-  "v1/edit/index.html"
+  "v1/edit/index.html",
+  "",
+  "v1/view/",
+  "v1/edit/"
 ];
